0a1b9014118bcc028fc6b9e138e3f8da6c0de70a
Case Khor <case@hellomoss.com>
2019-09-27 22:27:03 +0800

    02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
 #write your code here
+
+def add x,y
+  x+y
+end
+
+def subtract x,y
+  x-y
+end
+
+def sum numbers
+  total = 0
+  while numbers.length > 0
+    total += numbers.pop
+  end
+  total
+end
+
+def multiply a
+  m = 1
+  a.each do |x|
+    m *= x
+  end
+  return m
+end
+
+def power x,y
+  x**y
+end
+
+def factorial n
+    return (1..n).inject(:*) || 1
+  #end
+end
