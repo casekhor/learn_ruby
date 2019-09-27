4fb1227a9a7ac4604756176d0161b93d2c26d640
Case Khor <case@hellomoss.com>
2019-09-27 23:03:36 +0800

    03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,38 @@
 #write your code here
+
+
+def echo phrase
+  phrase
+end
+
+def shout phrase
+  phrase.upcase
+end
+
+def repeat (phrase,times=2)
+  say = ((phrase + " ") * times)[0...-1]
+end
+
+def start_of_word phrase,characters=1
+  phrase[0..characters-1]
+end
+
+def first_word phrase
+  phrase.split[0]
+end
+
+def titleize phrase
+  index = 0
+  phrase.split(" ").each do |value|
+    if index==0
+      value.capitalize!
+    else
+      if value.length >= 2 and value.length <=3
+        value.downcase!
+      else
+        value.capitalize!
+      end
+    end
+    index = index + 1
+  end.join(" ")
+end
